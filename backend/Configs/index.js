dd01835067faf6dcc0c49b6a6f5bca645a819557
ABF00711 <lwei99597@gmail.com>
2025-12-24 00:17:55 +0900

    refactor(index.js): add connectDB

--- backend/Configs/index.js
+++ backend/Configs/index.js
@@ -1,5 +1,6 @@
 const configs = {
     PORT: 5000,
+    dbURI: 'mongodb+srv://lwei99597_db_user:QuQAIK8Ud1RhwdRy@abf.10c2jq9.mongodb.net/'
 }
 
 module.exports = configs;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const app = require('./app.js');
 const configs = require('./Configs/index.js');
+const connectDB = require('./MongoDB/index.js');
 
+connectDB();
 app.listen(configs.PORT, () => {
   console.log("Server is running on port " + configs.PORT);
 });
