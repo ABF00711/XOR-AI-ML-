12c8e863b1f818f0c9df99c4fbc0e04fc41b9036
ABF00711 <lwei99597@gmail.com>
2025-12-23 10:52:26 +0900

    feat(perceptrons.js)

---
+++ backend/Models/perceptrons.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const perceptronSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    train_formula: {
+        type: String,
+        required: true
+    },
+    weights: {
+        type: [Number],
+        required: true
+    },
+    bias: {
+        type: Number,
+        required: true
+    },
+    user_id: {
+        type: String,
+        required: true
+    },
+});
+
+const PerceptronDB = mongoose.model('Perceptron', perceptronSchema);
+module.exports = PerceptronDB;
