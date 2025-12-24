0ea1227a4000b69ba5a672e2c384975c5a5d4b4f
ABF00711 <lwei99597@gmail.com>
2025-12-25 00:54:52 +0900

    feat(Dashboard): add dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import UserProvider from './Context';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ frontend/src/Pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+function Dashboard () {
+
+    return (
+        <div>
+            <h1>Welcome to the Dashboard</h1>
+            <p>This is a protected route. Only logged-in users can see this.</p>
+        </div>
+    );
+}
+
+export default Dashboard;
