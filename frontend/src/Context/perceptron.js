67248e555e6caa49aa6e65d06ed960081debf3c2
ABF00711 <lwei99597@gmail.com>
2025-12-25 19:24:58 +0900

    refactor(initData): modify function so an be converted to real function

--- frontend/src/Context/perceptron.js
+++ frontend/src/Context/perceptron.js
@@ -5,8 +5,11 @@ export const PerceptronContext = createContext([]);
 const initData = {
     name: "XOR Neural Network",
     description: "The XOR function outputs true only when the inputs differ. This seemingly simple operation was historically challenging for neural networks to learn, making it a perfect benchmark for testing AI capabilities.",
-    function: "f(x₁, x₂) = x₁ ⊕ x₂",
-    train_formula: "w + Δw = w + η(d - y)x",
+    function: `function f(x1, x2) {return x1 ^ x2;}`,
+    train_formula: 
+        `function getWeight(weight, learningRate, error, input) {
+            return weight + (learningRate * error * input);
+        }`,
     weights: [],
     bias: 1,
 }
